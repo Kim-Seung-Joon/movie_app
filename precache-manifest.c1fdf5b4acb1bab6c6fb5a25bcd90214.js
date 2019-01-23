5b62d7085073c89e7abf3ee8a047f304564e867c
Kim-Seung-Joon <wldhel9228@gmail.com>
2019-01-23 14:06:18 +0900

    Updates

---
+++ precache-manifest.c1fdf5b4acb1bab6c6fb5a25bcd90214.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d89b674b276ca8a2766",
+    "url": "/movie_app/static/css/main.5285d973.chunk.css"
+  },
+  {
+    "revision": "9d89b674b276ca8a2766",
+    "url": "/movie_app/static/js/main.9d89b674.chunk.js"
+  },
+  {
+    "revision": "22fdec2062f68dfa3e16",
+    "url": "/movie_app/static/js/1.22fdec20.chunk.js"
+  },
+  {
+    "revision": "f9521ac5b62e87334b5b",
+    "url": "/movie_app/static/js/runtime~main.f9521ac5.js"
+  },
+  {
+    "revision": "28bfc1cad7ba6ed5ee9c2608f7ed3b61",
+    "url": "/movie_app/index.html"
+  }
+];
